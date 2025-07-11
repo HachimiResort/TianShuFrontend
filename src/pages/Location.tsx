b9aa24a52c9af01c7006a8f3176091849984960d
wyq <1903910367@qq.com>
2025-07-11 16:04:22 +0800

    修复地图无法显示的bug

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { MapLocation } from "@/components/map/MapLocation"
+// import { MapLocation } from "@/components/map/MapLocation"
 import { MapTest } from "@/components/map/MapTest"
 import { ToastProvider } from "@/components/ui/toast"
 
